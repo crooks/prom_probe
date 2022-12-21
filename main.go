8036ce2d1a0ef2cb2eece9be48aab64e412cbfe1
Steve Crook <steve@stmellion.org>
2022-12-21 13:50:44 +0000

    Add samples

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	stdlog "log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/Masterminds/log-go"
 	"github.com/crooks/jlog"
 	loglevel "github.com/crooks/log-go-level"
 	"github.com/crooks/prom_probe/config"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -17,9 +19,42 @@ var (
 	flags *config.Flags
 )
 
+func probe(widgetName string) error {
+	sampleMetric.Set(float64(time.Now().Unix()))
+	sampleWidget.WithLabelValues(widgetName).Set(1)
+	return nil
+}
+
+func probeHandler(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	target := params.Get("target")
+	if target == "" {
+		http.Error(w, "Target parameter missing or empty", http.StatusBadRequest)
+		return
+	}
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(probeDuration)
+	registry.MustRegister(probeSuccess)
+	registry.MustRegister(sampleMetric)
+	registry.MustRegister(sampleWidget)
+	success := 1
+	start := time.Now()
+	err := probe("thingy")
+	if err != nil {
+		success = 0
+		log.Warnf("Probe of %s failed with %v", target, err)
+	}
+	duration := time.Since(start).Seconds()
+	probeSuccess.Set(float64(success))
+	probeDuration.Set(duration)
+	h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+	h.ServeHTTP(w, r)
+}
+
 func main() {
+	var err error
 	flags = config.ParseFlags()
-	cfg, err := config.ParseConfig(flags.Config)
+	cfg, err = config.ParseConfig(flags.Config)
 	if err != nil {
 		log.Fatalf("Cannot parse config: %v", err)
 	}
@@ -48,5 +83,8 @@ func main() {
 	}
 	initCollectors()
 	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/probe", func(w http.ResponseWriter, r *http.Request) {
+		probeHandler(w, r)
+	})
 	http.ListenAndServe("localhost:8080", nil)
 }
